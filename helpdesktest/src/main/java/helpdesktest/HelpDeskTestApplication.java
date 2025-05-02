caf5089cb3caa2ffb0ddf5fea6293a655c180bfe
marcus aurelio <marcusaurelio743@gmail.com>
2025-05-02 17:40:48 -0300

    criando Sistema de ordem de serviço

---
+++ helpdesktest/src/main/java/helpdesktest/HelpDeskTestApplication.java
@@ -0,0 +1,13 @@
+package helpdesktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskTestApplication.class, args);
+	}
+
+}
---
+++ helpdesktest/src/main/java/helpdesktest/domains/enumns/Perfil.java
@@ -0,0 +1,21 @@
+package helpdesktest.domains.enumns;
+
+public enum Perfil {
+	ADMIN(0,"ROLE_ADMIN"),CLIENTE(1,"ROLE_CLIENTE"),TECNICO(2,"ROLE_TECNICO");
+	
+		private Integer codigo;
+		private String descricao;
+		public Integer getCodigo() {
+			return codigo;
+		}
+		public String getDescricao() {
+			return descricao;
+		}
+		private Perfil(Integer codigo, String descricao) {
+			this.codigo = codigo;
+			this.descricao = descricao;
+		}
+		
+		
+
+}
